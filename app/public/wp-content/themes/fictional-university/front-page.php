f95b91e5e36c3687ebac0bd7e3b8fba1fc699384
Amelia Goodson <amelia.goodson@narrowcontent.com>
2023-07-27 14:53:18 -0400

    Metaquery for events on FP to sort by event date

--- app/public/wp-content/themes/fictional-university/front-page.php
+++ app/public/wp-content/themes/fictional-university/front-page.php
@@ -12,10 +12,24 @@
   </div>
 </div>
 <?php
+$today = date('Ymd');
 $eventQuery = get_posts(array(
   'post_type' => 'event',
-  'posts_per_page' => 2
-))
+  'posts_per_page' => -1,
+  // the custom field name
+  'meta_key' => 'event_date',
+  // order by the meta key
+  'orderby' => 'meta_value_num',
+  'order' => 'ASC',
+  // specify parameters for the custom field (meta query)
+  'meta_query' => array(
+    array(
+      'key' => 'event_date',
+      'compare' => '>=',
+      'value' => $today
+    )
+  )
+));
 ?>
 <div class="full-width-split group">
   <div class="full-width-split__one">
@@ -23,11 +37,22 @@ $eventQuery = get_posts(array(
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
       <?php foreach ($eventQuery as $post) {
+        $eventDate = get_field('event_date')
       ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php the_time('M') ?></span>
-            <span class="event-summary__day"><?php the_time('d') ?></span>
+            <span class="event-summary__month">
+              <?php
+              $date = new DateTime($eventDate);
+              echo $date->format('M')
+              ?>
+            </span>
+            <span class="event-summary__day">
+              <?php
+              $date = new DateTime($eventDate);
+              echo $date->format('d')
+              ?>
+            </span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
